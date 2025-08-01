d01a9b994da12803326b7544ef4723df86882a63
KauriXZ <palkovnik.gineral@yandex.ru>
2025-08-01 19:01:04 +0400

    Оптимизация вывода результата вычислений посредством строки, добавление возможности запоминания и воспроизведения предыдущего результата вычисления, небольшое изменение в значениях функции.

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-def calc(x,y):
+def calc(x,y,choose):
   if choose == '1':
     z = x + y
   elif choose == '2': 
@@ -9,21 +9,37 @@ def calc(x,y):
     if y != 0:
       z = x / y
     else:  
-      z = print('Делить на ноль нельзя! ')
-  return z
+      z = 'Делить на ноль нельзя! Попробуйте ещё раз.'
+  return str(z)
    
 
 
-
 print('Добро пожаловать в калькулятор!')
-print('1 - сложение 2 - вычитание 3 - умножение 4 - деление 0 - выход')
+print('1 - сложение 2 - вычитание 3 - умножение 4 - деление 0 - выход 00 - посмотреть предыдущие вычисления')
 while True: 
+  
+  #Выбор операции
   choose = input('Выберите нужную вам операцию: ')
-  if choose != '0':
+  
+
+  #Проведение вычислений и добавление их в историю
+  if choose != '0' and choose != '00':
     x = float(input('Введите первое число: '))
     y = float(input('Введите второе число '))
-    z = calc(x,y)
+    z = calc(x,y,choose)
     print(f'Ваш ответ: {z}')
+    with open('last_res.txt', 'w', encoding='utf-8') as file:
+      file.write(z)
+  
+  
+  #Просмотр результатов
+  elif choose == '00':
+    with open('last_res.txt', 'r', encoding='utf-8') as file:
+      last_results = file.read()
+      print(last_results)
+  
+
+  #Завершение работы
   else:
     print('До скорых встреч!')
     input('Нажмите Enter, чтобы выйти...')
